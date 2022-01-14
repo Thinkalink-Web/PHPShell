bd79a3c1e87ff952284067a6b71d0b5cb9cdf3c8
AlexTech01 <72891399+AlexTech01@users.noreply.github.com>
2022-01-14 16:04:03 -0600

    Create config.php

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+// Is PHPShell enabled?
+// If not true, PHPShell will not load
+// If true, PHPShell will load
+define("PHPSHELL_ENABLED", true);
+
+// The password used to sign in to PHPShell
+// Please change this password before enabling PHPShell
+define("PHPSHELL_PASSWORD", password_hash("phpshell", PASSWORD_DEFAULT));
+
+// Thanks for using PHPShell!
