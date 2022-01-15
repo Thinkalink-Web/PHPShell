085571169645e1a2122025884ead7d395df99a3e
AlexTech01 <72891399+AlexTech01@users.noreply.github.com>
2022-01-15 09:23:57 -0600

    Update index.php

--- index.php
+++ index.php
@@ -108,22 +108,8 @@ function SHOW_SHELL_HTML() {
       </form>
       <p id="output">The program produced no output</p>
       <script>
-        function getQueryStringValues(key) {
-          var arrParamValues = [];
-          var url = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-          
-          for (var i = 0; i < url.length; i++) {
-            var arrParamInfo = url[i].split('=');
-            
-            if (arrParamInfo[0] == key || arrParamInfo[0] == key+'[]') {
-              arrParamValues.push(decodeURIComponent(urlparam[1]));
-            }
-          }
-          
-          return (arrParamValues.length > 0 ? (arrParamValues.length == 1 ? arrParamValues[0] : arrParamValues) : null);
-        }
-        
-        document.getElementById("output").innerHTML = getQueryStringValues(key);
+        var objUrlParams = new URLSearchParams(window.location.search);
+        document.getElementById('output').innerHTML = objUrlParams.get('output');
       </script>
     </body>
   </html>
